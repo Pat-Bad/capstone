6acc60ccdea57306681a37c73b62fd9c9a2c9138
Patricia <patricia.badji89@gmail.com>
2025-02-25 11:56:52 +0100

    fixes bug

---
+++ src/main/java/it/epicode/capstone/playlists/PlaylistRepository.java
@@ -0,0 +1,7 @@
+package it.epicode.capstone.playlists;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+}
