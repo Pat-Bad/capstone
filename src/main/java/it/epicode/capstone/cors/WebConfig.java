dfdf190ec540d7aef82c3ebbb76e028711d7a1c2
Patricia <patricia.badji89@gmail.com>
2025-03-21 15:59:59 +0100

    adds functionalities

--- src/main/java/it/epicode/capstone/cors/WebConfig.java
+++ src/main/java/it/epicode/capstone/cors/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://patriciascapstone.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
